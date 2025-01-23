8024ea182a161c84f279f1c7e99922e15e5a6173
Akshata Jog <akshatacontact@gmail.com>
2025-01-23 12:16:19 +0400

    chapter9-class-completed

---
+++ chapter-9-class.js
@@ -0,0 +1,24 @@
+class Cat {
+  static species = "Felis catus";
+  #isSleeping;
+  paws = 4;
+  sound = "meow";
+
+  constructor(name, favoriteToy) {
+    this.name = name;
+    this.favoriteToy = favoriteToy;
+  }
+
+  static herd() {
+    throw new Error("You can't don that.");
+  }
+
+  play() {
+    console.log(`Playing with ${this.favoriteToy}`);
+    this.#takeNap();
+  }
+
+  #takeNap() {
+    this.#isSleeping = true;
+  }
+}
